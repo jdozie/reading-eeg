b65ac0ea7c2024835486d7131e6733facf329b1b
Jenna Dauzat <jenna.mchugh@selu.edu>
2022-06-07 17:57:21 -0500

    add annotations to the patient files. also added a results folder where the annotations were saved as CSV and a plotting method where annotations can be viewed on a plot.

--- readingeeg.py
+++ readingeeg.py
@@ -2,13 +2,152 @@
 """
 Created on Thu Apr  7 23:29:00 2022
 
-@author: racer
+@author: jenna
 """
 import mne
+import pathlib
+
+#see https://mne.tools/dev/auto_tutorials/raw/10_raw_overview.html#the-raw-info-attribute
+def print_patient_info(filename, raw):
+  # raw.info
+  n_time_stamps = raw.n_times
+  time_secs = raw.times
+  ch_names = raw.ch_names
+  n_chan = len(ch_names)      #note: there is no raw.n_channels attribute
+  print('the sample data object has {} time samples and {} channels.'''.format(n_time_stamps, n_chan))
+  print('the last time sample is at {} seconds.'.format(time_secs[-1]))
+  print('the first few channel names are {}'.format(', '.join(ch_names[:3])))
+  print()
+
+  print('bad channels: ', raw.info['bads'])  # channels marked "bad" during acquisition
+  print(raw.info['sfreq'], 'Hz')  # sampling frequency
+  print(raw.info['description'], '\n')  # misc acquisition info
+
+  print(raw.info)
+
+def annotate_patient_info(filename, raw):
+    patient_annotations = mne.Annotations(onset=
+                                          [0, 60, 61, 121, 122, 127, 128, 133, 134, 139, 140, 145, 146, 151, 152, 157,
+                                           158, 163, 164, 169, 170, 175, 176, 181, 182, 187, 188, 193, 194, 199, 200,
+                                           205,
+                                           206, 211, 212, 217, 218, 223, 224, 229, 230, 235, 236, 241, 242, 247, 248,
+                                           253,
+                                           254, 259, 260, 265, 266, 271, 272, 277, 278, 283, 284, 289, 290, 295, 296,
+                                           301,
+                                           302, 307, 308, 313, 314, 319, 320, 325, 326, 331, 332, 337, 338, 343, 344,
+                                           349,
+                                           350, 355, 356, 361, 362, 367, 368, 373, 374, 379, 380, 385, 386, 391, 392,
+                                           397,
+                                           398, 403, 404, 409, 410, 415, 416, 421, 422, 427, 428, 433, 434, 439, 440,
+                                           445,
+                                           446, 451, 452, 457, 458, 463, 464, 469, 470, 475, 476, 481, 482, 487, 488,
+                                           493,
+                                           494, 499, 500, 505, 506, 511, 512, 517, 518, 523, 524, 529, 530, 535, 536,
+                                           541,
+                                           542, 547, 548, 553, 554, 559, 560, 565, 566, 571, 572, 577, 578, 583, 584],
+                                          # , 589],
+                                          duration=
+                                          [60, 1, 60, 1, 5,  # eye open, eye close, A
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, B
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, C
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, D
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, E
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, F
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, G
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, H
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, I
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, J
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, K
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, L
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, M
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, N
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, O
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, P
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, Q
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, R
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, S
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, T
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, U
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, V
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, W
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, X
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, Y
+                                           1, 5, 1, 5, 1, 5,  # black screen x2, Z
+                                           1, 5, 1, 5  # black screen x2
+                                           ],
+                                          description=
+                                          ['Eye Open', 'transition', 'Eye Closed', 'transition', 'A',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'B',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'C',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'D',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'E',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'F',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'G',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'H',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'I',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'J',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'K',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'L',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'M',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'N',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'O',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'P',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'Q',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'R',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'S',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'T',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'U',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'V',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'W',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'X',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'Y',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen', 'transition',
+                                           'Z',
+                                           'transition', 'Black Screen', 'transition', 'Black Screen'
+                                           ])
+    raw.set_annotations(patient_annotations)
+    print(raw.annotations)
+
+    #print annotation information
+    for annotation in raw.annotations:
+        description = annotation['description']
+        start = annotation['onset']
+        end = annotation['onset'] + annotation['duration']
+        print("'{}' goes from {} to {}".format(description, start, end))
+
+    # #plot annotated data
+    # fig = raw.plot()
+    # #allow modifications to annotations from plot window
+    # fig.fake_keypress('a')
+
+    raw.annotations.save('results/{}.csv'.format(str(filename)), overwrite=True)
 
 if __name__ == "__main__":
-  patient1_raw_file = "EEG_Data/Subject_01/20220311213941_Patient01.nedf"
-  patient1_raw = mne.io.read_raw_nedf(patient1_raw_file)
-  print(patient1_raw)
-  print(patient1_raw.info)
-
+  for nedf_file in pathlib.Path('EEG_Data/').glob('**/*.nedf'):
+      patient_raw = mne.io.read_raw_nedf(str(nedf_file))
+      print_patient_info(nedf_file.stem, patient_raw)
+      annotate_patient_info(nedf_file.stem, patient_raw)
